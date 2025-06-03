74beeae42da8d4cc63415c499803d3bbb05449f5
JoeTwizzle <the3.14159268483279@gmail.com>
2025-06-03 19:59:29 +0200

    Add better outline

--- Assets/TerrainGen/TerrainManager.cs
+++ Assets/TerrainGen/TerrainManager.cs
@@ -2,13 +2,10 @@ using System;
 using System.Buffers;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static Unity.Collections.AllocatorManager;
 
 
 [Serializable]
